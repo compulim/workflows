b93522fba34cfe5419b2d938b5e240da8552cc8d
William Wong <compulim@hotmail.com>
2025-12-31 16:07:22 -0800

    Empty override

--- scaffold/common/copy/packages/$package-name/tsup.config.override.ts
+++ scaffold/common/copy/packages/$package-name/tsup.config.override.ts
@@ -2,10 +2,6 @@ import { type Options } from 'tsup';
 
 export default function override(options: Options): Options {
   return {
-    ...options,
-    entry: {
-      'react-chain-of-responsibility': './src/index.ts',
-      'react-chain-of-responsibility.preview': './src/index.preview.ts'
-    }
+    ...options
   };
 }
